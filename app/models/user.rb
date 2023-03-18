4d3bc12a537d2ee24dc3963637e8d5ad11909c7b
Ose-Augustine <osebeckley@gmail.com>
2023-03-18 03:29:37 +0100

    Added secure passwords.
    Gemfile include bcrypt
    Modify fixtures to new schema change

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    before_save {self.email = email.downcase}
+    validates :name, presence: true
+    validates :password, presence: true, length: {minimum: 6} 
+
+    has_secure_password 
+    validates :email, presence: true, uniqueness: { case_sensitive: false }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,10 +2,28 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
  def setup
-  @user = User.new(name:'emeka', email:'who', password:'hello')
+  @user = User.new(name:'emeka', email:'who', password:'hellos',password_confirmation:'hellos')
  end
 
  test "is user valid" do 
   assert @user.valid?
  end
+ test "Password must be provided" do 
+  @user.password = @user.password_confirmation = ''
+  assert @user.invalid? 
+ end
+ test "Password must be of minimum length" do 
+  @user.password = @user.password_confirmation = 'a' * 6 
+  assert @user.valid?
+ end 
+ test "Email must be provided" do 
+  @user.email = "" 
+  assert_not @user.valid?
+ end 
+ test "Email must be unique" do 
+  @user = User.new(name:'james', email:'who', password:'password',password_confirmation:'password')
+  assert @user.save 
+  assert user = User.new(name:'james', email:'who', password:'password',password_confirmation:'password')
+  assert user.invalid?
+ end  
 end
