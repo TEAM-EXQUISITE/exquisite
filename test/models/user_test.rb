c7668b968ec3a7102cd1c65ec089d3aa4fe8dc59
Ose-Augustine <osebeckley@gmail.com>
2023-03-18 02:31:05 +0100

    Updating fixutures to reflect schema change.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ def setup
+  @user = User.new(name:'emeka', email:'who', password:'hello')
+ end
+
+ test "is user valid" do 
+  assert @user.valid?
+ end
 end
