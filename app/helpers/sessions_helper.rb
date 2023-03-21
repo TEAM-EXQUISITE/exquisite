8f977c6975bcc66d97a3ea154947bbe6aa200640
Ose-Augustine <osebeckley@gmail.com>
2023-03-21 17:15:31 +0100

    Helper for logged in status.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,18 @@
 module SessionsHelper
+    def log_in(user)
+        #logs in the given user 
+        session[:user_id] = user.id 
+    end
+
+    def current_user 
+        if session[:user_id] 
+            #memoization 
+            #sets the current user if nil else returns it 
+            @current_user ||= User.find_by(id: session[:user_id])
+        end 
+    end 
+
+    def logged_in? 
+        !current_user.nil? 
+    end 
 end
