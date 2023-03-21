ba34d26975d657e6b647e2ae9e1dea62b9ee02a7
Ose-Augustine <osebeckley@gmail.com>
2023-03-21 08:54:55 +0100

    Sign in flow passes test.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create 
+    user = User.find_by(email: params[:session][:email]) 
+    if user && user.authenticate(params[:session][:password]) 
+      flash.now[:success] = "you have been verified" 
+      redirect_to user_path
+    else 
+      flash.now[:danger] = "wrong password/email combination" 
+      render :new
+    end 
+  end 
 end
