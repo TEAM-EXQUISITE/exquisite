f0ae932fa88308a663a2d598f71895b6318fec17
Ose-Augustine <osebeckley@gmail.com>
2023-03-21 17:40:38 +0100

    Integration test for user flow.

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+
+  def setup 
+    @user = users(:michael) 
+  end 
+
   test "Show flash message"  do 
     get new_session_url 
     assert_template "sessions/new" 
@@ -14,5 +19,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end 
+
+  test "Login with valid information" do 
+    get new_session_url
+    post sessions_path, params: {
+      session: {
+        email: @user.email, 
+        password: 'password'
+      }
+    }
+    assert_redirected_to @user 
+    follow_redirect!
+    assert_template 'users/show' 
   end 
 end
