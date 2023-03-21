00d50cf0a882d8347bd91be78d2803d3356f2450
Ose-Augustine <osebeckley@gmail.com>
2023-03-21 17:29:51 +0100

    Hash method for use in fixtures.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
 
     has_secure_password 
     validates :email, presence: true, uniqueness: { case_sensitive: false }
+
+    #Returns the hash digest of the given string
+    def User.digest(string) 
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                        BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost) 
+    end 
+    
 end
