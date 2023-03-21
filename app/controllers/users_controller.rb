f5f67c6fdb914fb5be270afcfdeaf7c96761d267
Ose-Augustine <osebeckley@gmail.com>
2023-03-21 08:29:30 +0100

    Setting up sessions view.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,10 @@ class UsersController < ApplicationController
       render :new , status: :unprocessable_entity
     end 
   end
+
+  def show 
+    @user = User.find(params[:id])
+  end
   
   private 
     def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root "users#index"
   resources :users 
+  resources :sessions, only: [:new, :create, :destory]
 end
