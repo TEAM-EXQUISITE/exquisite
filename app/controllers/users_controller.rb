2e666a6a8f5912db7608b47c060f70e002013835
Ose-Augustine <osebeckley@gmail.com>
2023-03-21 17:45:52 +0100

    Log in user after sign up.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params) 
     if @user.save 
+      log_in @user
       redirect_to @user
     else
       render :new , status: :unprocessable_entity
