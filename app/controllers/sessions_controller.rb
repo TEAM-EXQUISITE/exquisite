ec1bad9c1d85b376fa1651e01620f7dbf770f365
Ose-Augustine <osebeckley@gmail.com>
2023-03-24 06:05:17 +0100

    Form partial.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,4 +13,9 @@ class SessionsController < ApplicationController
       render :new, status: :unprocessable_entity
     end 
   end 
+
+  def destroy 
+    log_out 
+    redirect_to root_path
+  end 
 end
