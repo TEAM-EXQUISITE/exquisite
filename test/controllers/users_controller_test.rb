c53c2d6f8d00e3751cc5851abe44cdb5cfabee9c
Ose-Augustine <osebeckley@gmail.com>
2023-03-26 21:52:55 +0100

    Testing controller redirect for guest users.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,25 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup 
+    @user = users(:michael) 
+  end 
+
   test "should get new" do
     get users_path
     assert_response :success
+  end
+
+  test "Should redirect edit when not logged in" do 
+    get edit_user_url(@user) 
+    assert_not flash.empty? 
+    assert_redirected_to new_session_url 
+  end 
+
+  test "Should redirect update when not logged in" do 
+    patch user_url(@user) 
+    assert_not flash.empty? 
+    assert_redirected_to new_session_url 
   end
 end
