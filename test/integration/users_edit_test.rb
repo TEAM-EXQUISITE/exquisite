ccb4b225ecaf494c1f9f0a18470ca6f8d5ebce39
Ose-Augustine <osebeckley@gmail.com>
2023-03-27 02:02:03 +0100

    Test for friendly forwarding.
    Auto sign in a user before making edit.

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -32,4 +32,25 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     get edit_user_url(@user) 
     assert_not flash.empty? 
   end
+
+  test "Succesful edit with friendly forwarding" do 
+    get edit_user_path(@user) 
+    sign_in_as(@user,"password") 
+    assert_redirected_to edit_user_path(@user) 
+    name = "madison" 
+    email = "madison@email.com" 
+    patch user_path(@user), params: {
+      user: {
+        name: name, 
+        email: email, 
+        password: "password", 
+        password_confirmation: "password"
+      }
+    }
+    asset_not flash.emtpy? 
+    assert_redirected_to @user 
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end 
 end
